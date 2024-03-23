08e7863a902a857c447bbb2bbd86f3c5ce82e122
Marina <marinajalonzo1@gmail.com>
2024-03-23 18:17:04 -0500

    Middleware created.

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const activity = cwd.includes('social-network-challenge')
+  ? cwd.split('social-network-challenge')[1]
+  : cwd;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server for ${activity} running on port ${PORT}!`);
+    });
+  });
+
